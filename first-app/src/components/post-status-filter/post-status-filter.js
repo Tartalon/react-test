80355fbc6ad4fa8b3a62d85e61c5931235c4e88c
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-05-19 17:50:20 +0300

    react base

--- first-app/src/components/post-status-filter/post-status-filter.js
+++ first-app/src/components/post-status-filter/post-status-filter.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import './post-status-filter';
 
-const PostStastusFilter = () => {
-  return (
-    <div className="btn-group">
-      <Button color="info">Все</Button>
-      <button className="btn btn-info">Все</button>
-      <button className="btn btn-outline-secondary">Понравилось</button>
-    </div>
-  );
-};
+export default class PostStastusFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.buttons = [
+      { name: 'all', label: 'Все' },
+      { name: 'like', label: 'Понравилось' },
+    ];
+  }
 
-export default PostStastusFilter;
+  render() {
+    const buttons = this.buttons.map(({ name, label }) => {
+      const { filter, onFilterSelect } = this.props;
+      const active = filter === name;
+      const clazz = active ? 'btn-info' : 'btn-outline-secondary';
+      return (
+        <button
+          key={name}
+          type="button"
+          className={`btn ${clazz}`}
+          onClick={() => onFilterSelect(name)}
+        >
+          {label}
+        </button>
+      );
+    });
+    return <div className="btn-group">{buttons}</div>;
+  }
+}
