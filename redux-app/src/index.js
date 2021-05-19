f24996555e5b6fefdde44f780a9dd3cc019b57c3
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-05-19 23:55:19 +0300

    dispatch

---
+++ redux-app/src/index.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+store.dispatch({ type: "INC" });
+console.log(store.getState());
+
+// let state = reducer(undefined, {});
+
+// state = reducer(state, { type: "INC" });
+// console.log(state);
+// state = reducer(state, { type: "INC" });
+// console.log(state);
