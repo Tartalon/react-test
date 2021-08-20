f52fec0c180657957265fcdaba534b8ddd431603
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-08-20 19:00:57 +0300

    first app start

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,2 +1,18 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+import './index.css';
+
+function WhoAmI(props) {
+  return (
+    <>
+      <h1>
+        My name is {props.name}, surname - {props.surname}
+      </h1>
+      <a href={props.link}>My profile</a>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
